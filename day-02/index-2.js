2adab6d669e602e4c380331afc8d56fc009b3c59
Robert Baillie <robert.baillie@ortooapps.com>
2024-12-04 16:25:08 +0000

    day 2, part 2
    also, minor improvement to part 1

--- day-02/index-2.js
+++ day-02/index-2.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync( __dirname + '/input.txt', 'utf8' );
 
-const reportIsSafe = report => {
+const baseReportIsSafe = report => {
 
 	let direction = report[0] > report[1] ? -1 : 1;			// if the first number is the same as the second, it doesn't matter if we set the wrong directiom, the loop will fail it anyway
 
@@ -16,6 +16,23 @@ const reportIsSafe = report => {
 		}
 	}
 	return true;
+}
+
+const levelRemovedVariants = report => {
+	const levelRemovedVariants = [];
+	for ( let indexToRemove=0; indexToRemove<report.length; indexToRemove++ ) {
+		levelRemovedVariants.push( report.filter( ( value, index ) => index !== indexToRemove ) );
+	}
+	return levelRemovedVariants;
+}
+
+const aLevelRemovedVariantIsSafe = report => {
+	return levelRemovedVariants( report )
+				.some( baseReportIsSafe );
+}
+
+const reportIsSafe = report => {
+	return baseReportIsSafe( report ) || aLevelRemovedVariantIsSafe( report );
 }
 
 let safeCount = input.split('\n')

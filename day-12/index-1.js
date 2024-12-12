ed8f816aea738530e527b3e045e8d5cf6b16fc49
Robert Baillie <robert.baillie@ortooapps.com>
2024-12-12 19:23:29 +0000

    day 12, part 1 - ouch, that wasn't easy

---
+++ day-12/index-1.js
@@ -0,0 +1,97 @@
+console.time( 'initialisation' );
+
+const files = {
+	test: 'test-input.txt',
+	full: 'input.txt'
+}
+
+const file = files[ process.argv[2] ] || files.full;
+
+const fs = require('fs');
+const input = fs.readFileSync( __dirname + '/' + file, 'utf8' );
+
+const regionMap = [];
+const regionIndexes = [];
+
+const displacements = [
+	{ x: 0, y: -1 },
+	{ x: 1, y: 0 },
+	{ x: 0, y: 1 },
+	{ x: -1, y: 0 }
+];
+
+const getSurroundingPoints = ( regionIdentifier, x, y ) => {
+	return displacements
+			.reduce( ( surroundingPoints, displacement ) => {
+				const point = regionMap[ x + displacement.x ]?.[ y + displacement.y ];
+				if ( point?.regionIdentifier === regionIdentifier ) {
+					surroundingPoints.push( point );
+				}
+				return surroundingPoints;
+			}, [] );
+}
+
+const addToRegion = ( region, point ) => {
+
+	if ( point.region !== false ) {
+		return;
+	}
+	region.add( point );
+	point.region = region;
+
+	getSurroundingPoints( point.regionIdentifier, point.x, point.y )
+		.forEach( surroundingPoint => {
+			addToRegion( region, surroundingPoint );
+		});
+}
+
+const findRegion = point => {
+	// if we already have a region, then skip it
+	if ( point.region !== false ) {
+		return;
+	}
+
+	// otherwise, create a new region
+	const newRegion = new Set();
+	regionIndexes.push( newRegion );
+
+	addToRegion( newRegion, point );
+}
+
+input
+	.split( '\n' )
+	.map( ( row, y ) => {
+		row.split( '' )
+			.map( cell => cell )
+			.forEach( ( regionIdentifier, x ) => {
+				const point = {
+					x: x,
+					y: y,
+					regionIdentifier: regionIdentifier,
+					regionPerimeterTotal: 0,
+					region: false,
+				}
+
+				regionMap[ x ] = regionMap[ x ] || [];
+				regionMap[ x ][ y ] = point;
+			})
+	});
+
+console.timeEnd( 'initialisation' );
+console.time( 'processing' );
+
+regionMap.forEach( ( row ) => {
+	row.forEach( ( point ) => {
+		findRegion( point );
+		point.regionPerimeterTotal = 4 - getSurroundingPoints( point.regionIdentifier, point.x, point.y ).length;
+	});
+});
+
+console.log(
+	regionIndexes.reduce( ( total, region ) => {
+		const perimeterTotal = Array.from( region ).reduce( ( regionPerimeterTotal, point ) => regionPerimeterTotal + point.regionPerimeterTotal, 0 );
+		return total + ( region.size * perimeterTotal );
+	}, 0 )
+);
+
+console.timeEnd( 'processing' );
